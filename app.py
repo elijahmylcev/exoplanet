5f6e240ddbb7c71a1d3b49c54eb7639dd876e70b
Elijah Mylcev <elijahmylcev@gmail.com>
2022-09-12 22:34:59 +0300

    fig && fig.show()

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import pandas as pd
 import plotly.express as px
 import plotly.io as pol
 
+pol.renderers.default = 'browser'
+
 response = requests.get('http://asterank.com/api/kepler?query={}&limit=2000')
 df = pd.json_normalize(response.json())
 
 print(df.head())
 
-fig = 
+fig = px.scatter(df, x = 'RPLANET', y = 'TPLANET')
+fig.show()
