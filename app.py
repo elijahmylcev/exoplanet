5b27fd3e549a2dffa9457a6a16a016f6dd653294
Илья Мыльцев <imylcev@kmis.ru>
2022-09-13 12:50:02 +0300

    add slider

--- app.py
+++ app.py
@@ -15,7 +15,16 @@ df = pd.json_normalize(response.json())
 
 print(df.head())
 
-fig = px.scatter(df, x = 'RPLANET', y = 'TPLANET')
+fig = px.scatter(df, x = 'RPLANET', y = 'A')
+
+rplanet_selector = dcc.RangeSlider(
+  id='range-slider',
+  min=min(df['RPLANET']),
+  max=max(df['RPLANET']),
+  marks={5: '5', 10: '10', 20: '20'},
+  step=1,
+  value=[5, 10]
+)
 
 # fig.show()
 
@@ -23,9 +32,16 @@ app = dash.Dash(__name__)
 
 # Frontend
 app.layout = html.Div([
-  html.H1('ExoPlanets chart'),
+  html.H1('Planet Temperature ~ Distance from the Star'),
+  html.Div(rplanet_selector, style = {'width': '50%'}),
   dcc.Graph(figure=fig)
-])
+],
+style={
+  'margin': '0 auto',
+  'width': '70%',
+  'font-family': 'JetBrains Mono'
+}
+)
 
 if __name__ == '__main__':
   app.run_server(debug=True)
