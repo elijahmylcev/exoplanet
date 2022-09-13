e53f9ba3237b86199528ccb29b92414686c7d3ac
Илья Мыльцев <imylcev@kmis.ru>
2022-09-13 14:59:23 +0300

    add selecor as slider && create callback

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app = dash.Dash(__name__)
 app.layout = html.Div([
   html.H1('Planet Temperature ~ Distance from the Star'),
   html.Div(rplanet_selector, style = {'width': '50%'}),
-  dcc.Graph(figure=fig)
+  dcc.Graph(id='dist-temp-chart', figure=fig)
 ],
 style={
   'margin': '0 auto',
@@ -43,5 +43,18 @@ style={
 }
 )
 
+@app.callback(
+  dash.Output(component_id='dist-temp-chart', component_property='figure'),
+  dash.Input(component_id='range-slider', component_property='value')
+)
+#                          component_property 
+def update_dist_temp_chart(radius_range):
+  print(radius_range[0], radius_range[1])
+  chart_data = df[(df['RPLANET'] > radius_range[0]) & (df['RPLANET'] < radius_range[1])]
+  fig = px.scatter(chart_data, x='TPLANET', y='A')
+
+  return fig
+
 if __name__ == '__main__':
   app.run_server(debug=True)
+
